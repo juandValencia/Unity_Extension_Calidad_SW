5ff6c73a9946eabbfe6cb6d4daee4f726551f68a
Santiago Morales <santiago.morales.alv@gmail.com>
2020-09-02 22:43:53 -0500

    subida de base

---
+++ API - Character System/Character.cs
@@ -0,0 +1,215 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.AI;
+using CharacterSystem.Skills;
+using CharacterSystem.Composition;
+
+namespace CharacterSystem
+{
+    public enum CharacterState{
+        hide,
+        inSpawn,
+        active,
+        inDead
+    }
+
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(CapsuleCollider))]
+    [RequireComponent(typeof(Animator))]
+
+    [RequireComponent(typeof(DefenceCharacter))]
+    [RequireComponent(typeof(EnergyCharacter))]
+    [RequireComponent(typeof(LifeCharacter))]
+    [RequireComponent(typeof(SpeedCharacter))]
+    [RequireComponent(typeof(TenacyCharacter))]
+    [RequireComponent(typeof(CognitiveControlCharacter))]
+    public class Character : MonoBehaviour
+    {
+        #region attributes
+        [SerializeField]
+        [InspectorName("Name")]
+        private String characterName;
+        
+        public delegate void NotifyState(Character m_character, CharacterState state);
+        public event NotifyState e_NewState;
+        
+        private AttributeComposition m_basicAttributes;
+        private int level; 
+        [SerializeField][InspectorName("List of Skills")]
+        protected Skill[] skills;
+        private Character_Control m_controlPlayer;
+        private CharacterState state;
+
+        private Animator m_Animator;
+        private NavMeshAgent m_NavAgent;
+        private MovIA m_movIA;
+        private MovJoystick m_movJoystick;
+        #endregion
+
+        #region properties
+        public String CharacterName
+        {
+            get
+            {
+                return characterName;
+            }
+
+        }
+        public Animator Animator
+        {
+            get
+            {
+                return m_Animator;
+            }
+            
+        }
+        public NavMeshAgent NavAgent
+        {
+            get
+            {
+                return m_NavAgent;
+            }
+
+        }
+        public MovIA MovIA
+        {
+            get
+            {
+                return m_movIA;
+            }
+        }
+        public MovJoystick MovJoystick
+        {
+            get
+            {
+                return m_movJoystick;
+            }
+        }
+        public AttributeComposition Attributes
+        {
+            get
+            {
+                return m_basicAttributes;
+            }
+        }
+        public Skill[] Skills
+        {
+            get
+            {
+                return skills;
+            }
+        }
+        public Character_Control ControlPlayer
+        {
+            get
+            {
+                return m_controlPlayer;
+            }
+            set
+            {
+                m_controlPlayer = value;
+            }
+        }
+        public CharacterState State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                state = value;
+            }
+        }
+        #endregion
+
+        protected void Awake()
+        {
+            m_Animator = this.GetComponent<Animator>();
+            m_NavAgent = this.GetComponent<NavMeshAgent>();
+            try
+            {
+                m_movIA = this.gameObject.GetComponentInChildren<MovIA>();
+            }
+            catch {
+            }
+            try { 
+            m_movJoystick = this.gameObject.GetComponentInChildren<MovJoystick>();
+        }
+            catch {
+            }
+
+            skills = this.GetComponents<Skill>();
+
+            BuildAttributes();
+        }
+
+        public void BuildAttributes() {
+            m_basicAttributes = new AttributeComposition(this.GetComponent<LifeCharacter>(), this.GetComponent<EnergyCharacter>(),
+                this.GetComponent<DefenceCharacter>(), this.GetComponent<SpeedCharacter>(), this.GetComponent<TenacyCharacter>(),
+                this.GetComponent<CognitiveControlCharacter>());
+
+            m_basicAttributes.LifeCharacter.e_withoutLife -= Dead;
+            m_basicAttributes.LifeCharacter.e_withoutLife += Dead;
+        }
+
+        public void UpgradeLevel() { //a character start in 0 level
+            level++;
+            //skills[level-1].UpgradeLevel();
+        }
+
+        #region states
+
+        private void UpdateState(CharacterState t_state)
+        {
+            state = t_state;
+            m_Animator.SetTrigger("changeState");
+            try
+            {
+                e_NewState(this, t_state);
+            }
+            catch
+            {
+            }
+        }
+
+        /// <summary>
+        /// It's killed by its own life attributte
+        /// </summary>
+        private void Dead() {
+            level = 0;
+            m_Animator.SetTrigger("die");
+            UpdateState(CharacterState.inDead);
+        }
+
+        /// <summary>
+        /// It's invoke by the GAME MODE
+        /// </summary>
+        public void FinishDead()
+        {
+            UpdateState(CharacterState.hide);
+        }
+
+        /// <summary>
+        /// It's invoke by the GAME MODE
+        /// </summary>
+        public void Spawn() 
+        {
+            UpdateState(CharacterState.inSpawn);
+
+        }
+
+        /// <summary>
+        /// It's invoke by the ANIMATOR
+        /// </summary>
+        public void FinishSpawn() 
+        {
+            UpdateState(CharacterState.active);
+        }
+
+        #endregion
+
+    }
+
+
+}
---
+++ Extension/Character_ViewWindow.cs
@@ -0,0 +1,232 @@
+﻿using UnityEngine;
+using UnityEditor;
+using CharacterSystem;
+using CharacterSystem.Skills;
+
+namespace EditorView
+{
+    public class Character_ViewWindow : EditorWindow
+    {
+        [MenuItem("Window/NTC Workspace/Character")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(Character_ViewWindow));
+        }
+
+
+        bool showingComposition = false, showingSkills = false;
+        bool builtAttributes = false;
+        Rect r = Rect.zero;
+        Vector2 scrollPos;
+        Character character;
+
+        GUIStyle titleStyle = new GUIStyle(), subtitleStyle = new GUIStyle();
+
+
+        private void ConfigStyles()
+        {
+            titleStyle.fontStyle = FontStyle.Bold;
+            titleStyle.fontSize = 16;
+
+            subtitleStyle.fontStyle = FontStyle.Italic;
+            subtitleStyle.fontSize = 12;
+        }
+
+        public void Update()
+        {
+            Repaint();
+        }
+
+        void OnGUI()
+        {
+            ConfigStyles();
+            try
+            {
+                character = (Selection.activeObject as GameObject).GetComponent<Character>();
+            }
+            catch {
+                
+            }
+            GUILayout.Space(10);
+
+            if (character != null)
+            {
+                #region basic info
+                GUILayout.Label(" Basic info", titleStyle);
+
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Name: " + character.CharacterName);
+                GUILayout.Label("State: " + character.State);
+                GUILayout.EndHorizontal();
+                
+                GUILayout.Space(20);
+                #endregion
+
+                GUILayout.BeginArea(new Rect(8,50, position.width-8, position.height-50));
+                scrollPos = GUILayout.BeginScrollView(scrollPos, false, true);
+                #region composition
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Composition", titleStyle);
+                if (showingComposition)
+                {
+                    if (GUILayout.Button("Hide Composition"))
+                    {
+                        showingComposition = false;
+                        builtAttributes = false;
+                    }
+                    /////////////////////////////////////////7
+                    if (!builtAttributes)
+                    {
+                        if (GUILayout.Button("Build Composition"))
+                        {
+                            character.BuildAttributes();
+                            builtAttributes = true;
+                        }
+
+                        GUILayout.EndHorizontal();
+                        GUILayout.Space(10);
+                    }
+                    else
+                    {
+                        GUILayout.EndHorizontal();
+                        GUILayout.Space(10);
+
+                        #region life
+                        GUILayout.Label("Life", subtitleStyle);
+                        GUILayout.BeginHorizontal();
+                        GUILayout.Label("Base: " + character.Attributes.LifeCharacter.BaseValue);
+                        GUILayout.Label("Real Value: " + character.Attributes.LifeCharacter.Value);
+                        GUILayout.Label("Regen Rate: " + character.Attributes.LifeCharacter.RegenerationRate);
+                        GUILayout.Label("Current: " + character.Attributes.LifeCharacter.CurrentValue);
+                        GUILayout.EndHorizontal();
+
+                        r = EditorGUILayout.BeginVertical();
+                        EditorGUI.ProgressBar(r, (character.Attributes.LifeCharacter.CurrentValue / character.Attributes.LifeCharacter.Value), "Current");
+                        GUILayout.Space(30);
+                        EditorGUILayout.EndVertical();
+                        GUILayout.Space(15);
+                        #endregion
+
+                        #region energy
+                        GUILayout.Label("Energy", subtitleStyle);
+                        GUILayout.BeginHorizontal();
+                        GUILayout.Label("Base: " + character.Attributes.EnergyCharacter.BaseValue);
+                        GUILayout.Label("Real Value: " + character.Attributes.EnergyCharacter.Value);
+                        GUILayout.Label("Regen Rate: " + character.Attributes.EnergyCharacter.RegenerationRate);
+                        GUILayout.Label("Current: " + character.Attributes.EnergyCharacter.CurrentValue);
+                        GUILayout.EndHorizontal();
+
+                        r = EditorGUILayout.BeginVertical();
+                        EditorGUI.ProgressBar(r, (character.Attributes.EnergyCharacter.CurrentValue / character.Attributes.EnergyCharacter.Value), "Current");
+                        GUILayout.Space(30);
+                        EditorGUILayout.EndVertical();
+                        GUILayout.Space(15);
+                        #endregion
+
+                        #region defence
+                        GUILayout.Label("Defence", subtitleStyle);
+                        GUILayout.BeginHorizontal();
+                        GUILayout.Label("Base: " + character.Attributes.DefenceCharacter.BaseValue);
+                        GUILayout.Label("Real Value: " + character.Attributes.DefenceCharacter.Value);
+                        GUILayout.EndHorizontal();
+                        GUILayout.Space(15);
+                        #endregion
+
+                        #region speed
+                        GUILayout.Label("Speed", subtitleStyle);
+                        GUILayout.BeginHorizontal();
+                        GUILayout.Label("Base: " + character.Attributes.SpeedCharacter.BaseValue);
+                        GUILayout.Label("Real Value: " + character.Attributes.SpeedCharacter.Value);
+                        GUILayout.EndHorizontal();
+                        GUILayout.Space(15);
+                        #endregion
+
+                        #region tenacy
+                        GUILayout.Label("Tenacy", subtitleStyle);
+                        GUILayout.BeginHorizontal();
+                        GUILayout.Label("Base: " + character.Attributes.TenacyCharacter.BaseValue);
+                        GUILayout.Label("Real Value: " + character.Attributes.TenacyCharacter.Value);
+                        GUILayout.EndHorizontal();
+                        GUILayout.Space(15);
+                        #endregion
+
+                        #region Cognitive Control
+                        GUILayout.Label("Cognitive Control", subtitleStyle);
+                        GUILayout.BeginHorizontal();
+                        GUILayout.EndHorizontal();
+                        GUILayout.Space(15);
+                        #endregion
+
+                    }
+                }
+                else
+                {
+                    if (GUILayout.Button("Show Composition"))
+                    {
+                        showingComposition = true;
+                    }
+                    GUILayout.EndHorizontal();
+                }
+                GUILayout.Space(20);
+                #endregion
+
+                #region Skills
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Skills info", titleStyle);
+                if (showingSkills)
+                {
+
+                    if (GUILayout.Button("Hide Skills"))
+                    {
+                        showingSkills = false;
+                    }
+                    GUILayout.EndHorizontal();
+                    GUILayout.Space(10);
+                    /////////////////////////////////////////7
+
+                    foreach (Skill t_skill in character.Skills)
+                    {
+                        GUILayout.BeginHorizontal();
+                        GUILayout.Label(t_skill.SkillName, subtitleStyle);
+                        GUILayout.Label("Level: " + t_skill.LevelSkill);
+                        GUILayout.EndHorizontal();
+
+                        GUILayout.BeginHorizontal();
+                        GUILayout.Label("Input | " + t_skill.Input);
+                        GUILayout.Label("Input name | " + t_skill.InputName);
+                        GUILayout.EndHorizontal();
+
+                        GUILayout.Label("State : " + t_skill.State);
+
+                        GUILayout.Label("Energy necessary : " + t_skill.Energy);
+
+                        GUILayout.BeginHorizontal();
+                        GUILayout.Label("Current cooldown : " + t_skill.CurrentCooldown);
+                        GUILayout.Label("Cooldown : " + t_skill.Cooldown);
+                        GUILayout.EndHorizontal();
+
+
+                        Rect r = EditorGUILayout.BeginVertical();
+                        EditorGUI.ProgressBar(r, (t_skill.CurrentCooldown / t_skill.Cooldown), "Current");
+                        GUILayout.Space(30);
+                        EditorGUILayout.EndVertical();
+
+                        GUILayout.Space(15);
+                    }
+                }
+                else
+                {
+                    if (GUILayout.Button("Show Skills"))
+                    {
+                        showingSkills = true;
+                    }
+                    GUILayout.EndHorizontal();
+                }
+                #endregion
+                GUI.EndScrollView();
+                GUILayout.EndArea();
+                
+            }
+        }
+    }
+}
